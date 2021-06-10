fad1570af0ba1255117c1e6935da6fb0bb9f6cf5
geodimitrov <geo.dimitrov@yahoo.com>
2021-06-10 22:07:21 +0300

    Added models & settings for Postgres

--- TodoApp/TodoApp/settings.py
+++ TodoApp/TodoApp/settings.py
@@ -67,8 +67,11 @@ WSGI_APPLICATION = 'TodoApp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'testdb',
+        'NAME': 'postgres',
         'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
